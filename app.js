2e1ea44b9d81eb16fcef4babae8eadf972efaf0e
bhargav <karandave503@gmail.com>
2023-07-04 11:19:13 +0530

    chang production and development mode in packej.json

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
 import express from "express";
 import userrouter from "./routes/user.js";
-import taskrouter from './routes/task.js'
+import taskrouter from "./routes/task.js";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import { isAuthenticated } from "./middlewares/auth.js";
 import { errormiddleware } from "./middlewares/error.js";
-import cors from 'cors'
+import cors from "cors";
 
-export const  app = express();
+export const app = express();
 const router = express.Router();
 app.use(express.json());
 app.use(cookieParser());
 
-
- 
- 
-
-
-
-
-app.use("/api/v1/users" , userrouter);
-app.use("/api/v1/task" , taskrouter);
+app.use("/api/v1/users", userrouter);
+app.use("/api/v1/task", taskrouter);
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
-)
-
-
- 
+);
 
 app.get("/", (req, res) => {
   res.send("welcome");
 });
 
-
- app.use(errormiddleware)
- 
+app.use(errormiddleware);
