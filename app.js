bab5d37042abd7a9310a02bf7d3378d5d46545ed
bhargav <karandave503@gmail.com>
2023-07-04 11:41:31 +0530

    add some in git

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import { isAuthenticated } from "./middlewares/auth.js";
 import { errormiddleware } from "./middlewares/error.js";
-import cors from "cors";
+import cors from "cors"; 
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 export const app = express();
 const router = express.Router();
@@ -15,13 +16,28 @@ app.use(cookieParser());
 app.use("/api/v1/users", userrouter);
 app.use("/api/v1/task", taskrouter);
 app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
+  '/api',
+  createProxyMiddleware({
+    target: 'https://todobackend-77ty.onrender.com',
+    changeOrigin: true,
+    secure: false,
+    headers: {
+      'Access-Control-Allow-Origin': 'http://localhost:5173',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
   })
 );
+
+app.use(cors())
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//     credentials: true,
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("welcome");
